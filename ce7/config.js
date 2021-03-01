f8fb95c2f6f3e9d0d272b4d97e01905b767c1cbc
acotest989 <acotest989@gmail.com>
2021-03-01 14:50:45 +0100

    C:/Program Files/Git/

--- ce7/config.js
+++ ce7/config.js
@@ -1,31 +1,32 @@
-const userType = 'doi';
-const contentType = 'adult'; // familySafe
-const apiKey = 'AB930-9CA333-74474C7-CCED20-4DEC-E13A';
-const redirection = false;
-const flow = true;
-let datingApp = 'https://dev.mojtajnisastanak.com';
-const cookieOverlay = false;
+const userType              = 'soi';
+const contentType           = 'adult'; // familySafe
+const apiKey                = 'AB930-9CA333-74474C7-CCED20-4DEC-E13A';
+let datingApp               = 'https://dev.mojtajnisastanak.com';
+let regRoute                = '/reg1';
+const redirection           = true;
+const flow                  = true;
+const cookieBar             = false;
+
 
 //terms and privacy variables
-const companyName = 'New Performance Group LTD';
-const companyAddress = '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius';
-const companyRegNumber = '129712';
-const complianceEmail = 'supp23@';
+const companyName           = 'New Performance Group LTD';
+const companyAddress        = '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius';
+const companyRegNumber      = '129712';
+const complianceEmail       = 'supp23@';
+
 
 // choose questions to render
-const use_ageRangeQ         = true;
-const use_genderQ           = true;
-const use_ageRange2Q        = true;
-const use_cityQ             = true;
-const use_relationShipQ     = true;
-const use_eyesColorQ        = true;
-const use_hairColorQ        = true;
-const use_skinColorQ        = true;
-const use_physiqueLookQ     = true;
-const use_heightQ           = true;
-const use_userPassQ         = true;
-const use_ageQ              = true;
-const use_emailQ            = true;
-const use_thanksQ           = true;
-const use_regStepQ          = false;
-const use_finalQ            = true;
+use_genderQ           = true;
+use_ageRangeQ         = true;
+use_cityQ             = true;
+use_relationShipQ     = true;
+use_eyesColorQ        = true;
+use_hairColorQ        = true;
+use_skinColorQ        = true;
+use_physiqueLookQ     = true;
+use_heightQ           = true;
+use_userPassQ         = true;
+use_ageQ              = true;
+use_emailQ            = true;
+use_thanksQ           = true;
+use_finalQ            = true;
