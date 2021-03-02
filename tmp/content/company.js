c28f43573ca6f5f30a188e32d6d84f97de8c1192
acotest989 <acotest989@gmail.com>
2021-03-02 13:50:18 +0100

    C:/Program Files/Git/

---
+++ tmp/content/company.js
@@ -0,0 +1,105 @@
+const companyData = {
+    EN: {
+        adult: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+        familySafe: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+    }, 
+    HR: {
+        adult: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+        familySafe: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+    },
+    CZ: {
+        adult: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+        familySafe: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+    },
+    CY: {
+        adult: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+        familySafe: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+    },
+    UK: {
+        adult: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+        familySafe: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+    },
+    SK: {
+        adult: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+        familySafe: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+    },
+    SI: {
+        adult: {
+            companyName: 'New Performance Group LTD',
+            companyAddress: '1st Floor, The Exchange, 18 Cybercity, Ebene, Mauritius',
+            companyRegNumber: '129712',
+            complianceEmail: 'supp23@',
+        },
+        familySafe: {
+            companyName: 'BGSmartBiznesEood',
+            companyAddress: '/',
+            companyRegNumber: '/',
+            complianceEmail: '/',
+        },
+    },
+};
+
+const companyName        = companyData[cCode][contentType]['companyName'];  
+const companyAddress     = companyData[cCode][contentType]['companyAddress'];  
+const companyRegNumber   = companyData[cCode][contentType]['companyRegNumber'];  
+const complianceEmail    = companyData[cCode][contentType]['complianceEmail'];  
